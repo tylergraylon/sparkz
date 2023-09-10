4bc391765230fce683ea465d9f959f1aaf66eca7
Victor Bello <you@example.com>
2023-09-10 12:13:27 +0100

    xfggv

--- src/app/components/TokenomicsMeter.tsx
+++ src/app/components/TokenomicsMeter.tsx
@@ -12,16 +12,20 @@ export default function TokenomicsMeter({
   gradPercent,
 }: Props) {
   return (
-    <div className="grid grid-cols-7 lg:grid-cols-15 text-textshade text-base md:text-lg">
+    <div className="grid grid-cols-6 text-textshade text-base md:text-lg content-start">
       <div
-        className={`${gradPercent} col-span-3 lg:col-span-7 h-5 bg-gradient-to-r from-transparent ${color} justify-self-end`}
+        className={`${gradPercent} col-span-2 sm:col-span-3 h-5 bg-gradient-to-r from-transparent ${color} justify-self-end`}
       ></div>
 
-      <span className="ml-2 text-sm sm:text-base">{percent}</span>
+      <div className="col-span-4 sm:col-span-3 grid grid-cols-8">
+        <span className="ml-2 text-sm sm:text-base col-span-2 md:col-span-1">
+          {percent}
+        </span>
 
-      <span className="text-textshade  text-opacity-80 font-mono col-span-3 lg:col-span-7 ml-3 sm:ml-0">
-        {text}
-      </span>
+        <span className="text-textshade text-opacity-80 font-mono col-span-6 md:col-span-7 ml-3 md:ml-6 lg:ml-3">
+          {text}
+        </span>
+      </div>
     </div>
   );
 }
